af3a6a9d6c8640304e89fcf6c1e9b80af7b7825e
Yeff <76024824+YeffyCodeGit@users.noreply.github.com>
2021-05-07 23:52:16 +0600

    Added package installing functionality

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from os import path
+import subprocess
+import sys
 import json
 
 CONFIG_FILE_NAME = "pyconfig.json"
-
 
 def get_config():
     if path.exists(CONFIG_FILE_NAME):
@@ -12,7 +13,15 @@ def get_config():
     else:
         print('fatal: No pyconfig.json file found.')
 
-
 config = get_config()
-print(f"Project name: {config['title']}")
-print(f'Project version: {config["version"]}')
+
+def install_pkgs():
+    try:
+     print(f'Installing dependancies for {config["title"]}')
+     for pkg in config["dependencies"]:
+         subprocess.check_call([sys.executable, "-m", "pip", "install", pkg])
+    except Exception as e:
+        print('fatal: Failed to import files')
+        print(e)
+
+install_pkgs()
