dc29d8e628e354de3094eeb60f23d2be42fa0ec2
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-11-03 14:36:43 +0600

    Project restart

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from configsetup import setup
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--setup", help="Use PyBuild to bootstrap your python project and generate a pyconfig file.", action="store_true")
-parser.add_argument("-i", "--install", help="Use PyBuild to update your project dependencies.", action="store_true")
-args = parser.parse_args()
-
-if args.setup:
-    setup()
+print("Hello World")
