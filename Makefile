327dfbb544dc9c0938a1c97b811a2864bcf59990
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-11-04 17:10:58 +0600

    Added Makefile and requirements file

---
+++ Makefile
@@ -0,0 +1,11 @@
+gui:
+	python . -gui
+
+terminal:
+	python .
+
+setup:
+	  pip install -r requirements.txt
+
+clean:
+	rm -rf __pycache__
--- gui.py
+++ gui.py
@@ -18,13 +18,16 @@ dpg.create_context()
 dpg.create_viewport(title="PyBuild", width=500, height=500)
 dpg.bind_theme(Themes.classic)
 
+
 def _create_project(sender, data):
 	...
+
 
 def create_new_project_dialog():
 	with dpg.window(label="Project Manager", width=485, height=480, no_move=True, pos=(0, 0)):
 		dpg.add_input_text(label="Project name", width=350, default_value="Hello, world!")
 		dpg.add_button(label="Create", callback=_create_project)
+
 
 def start_gui():
 	dpg.setup_dearpygui()
